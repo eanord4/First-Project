8af9f356bb491104d8dec15e933c5fd52961b142
Eric Nordstrom <eanord4@gmail.com>
2020-01-07 12:20:01 -0600

    Complete tests formatting in the initial comments

--- test_dataset_format.py
+++ test_dataset_format.py
@@ -1,28 +1,52 @@
-# script to convert raw text datasets from drugs.com into csv
+# script to confirm formatting of text data taken from drugs.com
+# formatting observed upon inspection:
+#   columns separated by tabs
+#   first line - header
+#   odd-numbered lines: rank, then drug name
+#   even-numbered lines:
+#       USUALLY company, value (sales or units), then TWO tabs, then the word "Stats"
+#       sometimes the company name is left out, in which case THE FIRST TAB IS ALSO LEFT OUT
+#       sometimes the value is left out, which does not remove a tab (this can result in 3 tabs in a row)
+#       STRATEGY: count tabs FROM THE RIGHT SIDE
+#
+# RESULT: all datasets pass this test!
 
-import os, csv
 
-sales_base_path = os.path.join('drugs.com datasets', 'raw', 'sales ')
-units_base_path = os.path.join('drugs.com datasets', 'raw', 'units ')
+import os
 
-with open(sales_base_path + '2013.txt', 'r') as f:
 
-    header = next(f)
-    even = False
+def sales_path(year):
+    return os.path.join('drugs.com datasets', 'raw', f'sales {year}.txt')
 
-    for line in f:
-        
-        tab_indices = []
+def units_path(year):
+    return os.path.join('drugs.com datasets', 'raw', f'units {year}.txt')
 
-        for i, char in enumerate(line):
-            if char == '\t':
-                tab_indices.append(i)
-        
-        # confirm characteristics of odd and even lines
-        if even and (
-            len(tab_indices) != 3
-            or tab_indices[1] != tab_indices[2] - 1  # 2nd and 3rd tabs adjacent
-            or 
-        ):
 
-        even = not even
+for path_func in sales_path, units_path:
+    for year in 2011, 2012, 2013:
+
+        path = path_func(year)
+
+        with open(path, 'r') as f:
+
+            filename = os.path.split(path)[-1]
+            print(f'Analyzing {filename}...')
+            header = next(f)
+            even = False
+
+            for line in f:
+                
+                entries = line.split('\t')
+
+                if even and (
+                    entries[-1] != 'Stats\n'
+                    or entries[-2] != ''
+                    or entries[-3] and not entries[-3].replace(',', '').isnumeric()
+                ) or not even and (
+                    not entries[0].isnumeric()
+                    or not entries[1].strip()  # check whether something actually included for the drug name
+                ):
+
+                    print('\t' + repr(line))
+
+                even = not even
