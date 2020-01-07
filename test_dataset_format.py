3272ac209722038d28a10c8f1b66d57459373033
Eric Nordstrom <eanord4@gmail.com>
2020-01-07 15:02:22 -0600

    Refactor test_datset_format.py to use more functions

--- test_dataset_format.py
+++ test_dataset_format.py
@@ -5,8 +5,9 @@
 #   columns separated by tabs
 #   first line - header
 #   odd-numbered lines: rank, then drug name
+#       ASSUMPTION: ranks go 1 by 1
 #   even-numbered lines:
-#       USUALLY company, value (sales or units), then TWO tabs, then the word "Stats"
+#       USUALLY company, value (sales or units), then TWO tabs, then the word Stats
 #       sometimes the company name is left out, in which case THE FIRST TAB IS ALSO LEFT OUT
 #       sometimes the value is left out, which does not remove a tab (this can result in 3 tabs in a row)
 #       STRATEGY: count tabs FROM THE RIGHT SIDE
@@ -23,31 +24,50 @@ def sales_path(year):
 def units_path(year):
     return os.path.join('drugs.com datasets', 'raw', f'units {year}.txt')
 
+def test_file(path_func, year):
+    
+    path = path_func(year)
+    filename = os.path.split(path)[-1]
+    print(f'Analyzing {filename}...')
 
-for path_func in sales_path, units_path:
-    for year in 2011, 2012, 2013:
+    with open(path, 'r') as f:
+        
+        _ = next(f)
+        even = False
+        last_rank = 0
 
-        path = path_func(year)
+        for line in f:
+            
+            entries = line.split('\t')
 
-        with open(path, 'r') as f:
+            # test whether formatting fails
+            if even and (
+                entries[-1] != 'Stats\n'
+                or entries[-2] != ''
+                or entries[-3] and not entries[-3].replace(',', '').isnumeric()
+            ) or not even and (
+                not entries[0].isnumeric()
+                or not entries[1].strip()  # check whether something actually included for the drug name
+            ):
+                print('\t' + repr(line))
 
-            filename = os.path.split(path)[-1]
-            print(f'Analyzing {filename}...')
-            header = next(f)
-            even = False
+            # test whether rankings are 1 by 1
+            elif not even:
 
-            for line in f:
+                rank = int(entries[0])
+
+                if rank != last_rank + 1:
+                    print(f'\t({last_rank})-->{repr(line)}')
                 
-                entries = line.split('\t')
+                last_rank = rank
 
-                if even and (
-                    entries[-1] != 'Stats\n'
-                    or entries[-2] != ''
-                    or entries[-3] and not entries[-3].replace(',', '').isnumeric()
-                ) or not even and (
-                    not entries[0].isnumeric()
-                    or not entries[1].strip()  # check whether something actually included for the drug name
-                ):
-                    print('\t' + repr(line))
+            even = not even
 
-                even = not even
+def main():
+    for path_func in sales_path, units_path:
+        for year in 2011, 2012, 2013:
+            test_file(path_func, year)
+
+
+if __name__ == '__main__':
+    main()
