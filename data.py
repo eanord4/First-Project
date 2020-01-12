2195afa02d9ec2387cec8c948ed36a062fd20b6a
Eric Nordstrom <eanord4@gmail.com>
2020-01-12 12:19:19 -0600

    Improve file path handling in data.py

--- data.py
+++ data.py
@@ -10,11 +10,14 @@ from weather import Weather
 from crime import Crime
 
 
-crime_path = "crime_data.csv"
-weather_path = "weather_data.csv"
-last_day_path = "last_day.txt"
-synth_path = "combined_data.csv"
-crime_header = "incident_no,description,code,family_violence,occur_date,report_datetime,loc_type,zip"  ###
+crime_filename = "crime_data.csv"
+weather_filename = "weather_data.csv"
+last_day_filename = "last_day.txt"
+synth_filename = "combined_data.csv"
+data_folder = "data"
+
+###
+crime_header = "incident_no,description,code,family_violence,occur_date,report_datetime,loc_type,zip"
 weather_header = "cloud_cover,dew_pt,humidity,precip_intensity,precip_prob,pressure,summary,temp,timestamp,uv,visibility,windspeed"  # leaves out icon and wind bearing
 
 
@@ -34,17 +37,20 @@ def hour(crimedt):
 
     return dt.datetime(year, month, day, hour)
 
-def retrieve(crime_path=crime_path, weather_path=weather_path):
+def retrieve(crime_filename=crime_filename, weather_filename=weather_filename, last_day_filename=last_day_filename, data_folder=data_folder):
     """loop to retrieve as much crime and weather data as possible"""
 
     obj_crime = Crime()
     obj_weather = Weather()
     one_day = dt.timedelta(1)
+    crime_path = os.path.join(data_folder, crime_filename)
+    weather_path = os.path.join(data_folder, weather_filename)
+    day_path = os.path.join(data_folder, last_day_filename)
 
-    if last_day_path in os.listdir():
+    if last_day_filename in os.listdir(data_folder):
         # initialize day based on previously saved value
 
-        with open(last_day_path, 'r') as dayfile:
+        with open(day_path, 'r') as dayfile:
             ordinal = int(dayfile.readline())
         
         day = dt.date.fromordinal(ordinal)
@@ -53,45 +59,53 @@ def retrieve(crime_path=crime_path, weather_path=weather_path):
         # initialize day as today
         day = dt.date.today()
     
-    with open(crime_path, 'a') as crimefile, open(weather_path, 'a') as weatherfile:
+    print(f"Collecting data starting from day={day}...\n")
+    
+    with open(crime_path, 'a') as crimefile, open(weather_path, 'a') as weatherfile, open(day_path, 'w') as dayfile:
         
         try:
             while True:  # end once bad response received
 
-                # get data
+                # get crime data
                 crimedf = obj_crime.get_crimes(day)
-                weatherdf = obj_weather.get_weather(day)
-
-                # save data for the day
                 crimedf.to_csv(crimefile, header=False)
-                weatherdf.to_csv(weatherfile, header=False)
+
+                # get weather data only if crimes found
+                if len(crimedf):
+                    weatherdf = obj_weather.get_weather(dt.datetime.combine(day, dt.datetime.min.time()))
+                    weatherdf.to_csv(weatherfile, header=False)
                 
                 # go back one day
                 day -= one_day
 
         except:
-            print()
             traceback.print_exc()
+            print()
         
         # save day on which error occurred
+        print(f"Saving break point at day={day}...\n")
+        dayfile.write(str(day.toordinal()))
 
-        print('\nSaving break point...')
-        
-        with open(last_day_path, 'w') as dayfile:
-            dayfile.write(str(day.toordinal()))
+def synthesize(crime_filename=crime_filename, weather_filename=weather_filename, synth_filename=synth_filename, data_folder=data_folder):
+    """join the datasets on the hour and save to the `synth_filename`"""
 
-def synthesize(crime_path=crime_path, weather_path=weather_path, synth_path=synth_path):
-    """join the datasets on the hour and save to the `synth_path`"""
+    crime_path = os.path.join(data_folder, crime_filename)
+    weather_path = os.path.join(data_folder, weather_filename)
+    synth_path = os.path.join(data_folder, synth_filename)
 
     # import previously retrieved datasets; add 'hour' column to crime dataset
-    crimedf = pd.read_csv(crime_path).assign(timestamp=lambda df: df.report_datetime.apply(day))
+    print('Loading previously saved data...\n')
+    crimedf = pd.read_csv(crime_path).assign(hour=lambda df: df.report_datetime.apply(hour))
     weatherdf = pd.read_csv(weather_path)
 
     # join and save
-    crimedf.merge(weatherdf, left_on='hour', right_on='timestamp').to_csv(synth_path)
+    print('Combining...\n')
+    crimedf.merge(weatherdf, left_on='hour', right_on='timestamp', how='left').to_csv(synth_path, index=False)
+
 
 if __name__ == '__main__':
     retrieve()
+    synthesize()
 
 
 
